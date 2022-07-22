3bed3c24043401f58ea847b27b10c46dccce1d6c
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-07-22 20:56:31 +0100

    ✅ Made major updates

--- src/App.js
+++ src/App.js
@@ -1,162 +1,210 @@
-import image from './img/bot_image.jpg';
-import './App.css';
-import { useState} from 'react';
+import "./App.css";
+import image from "./img/bot_image.jpg";
+import { useState, useRef } from "react";
 
 function App() {
+  const humanMessage = useRef();
+  const botmessage = useRef();
+  const input = useRef();
+
   const date = new Date();
   const hours = date.getHours();
-  const minutes = date.getMinutes();
   const seconds = date.getSeconds();
   const day = date.getDay();
   const month = date.getMonth();
   const year = date.getFullYear();
 
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const [time, setTime] = useState(`${hours}:${minutes}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
-  const [dateTime, setDateTime] = useState(`${days[day]}, ${day} ${months[month]} ${year}`); //using the useState hook to get the data from the local date and set it to the dateTime variable
-  // console.log(dateTime);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [time, setTime] = useState(`${hours}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
+  const [dateTime, setDateTime] = useState(
+    `${days[day]}, ${months[month]} ${year}`
+  ); //using the useState hook to get the data from the local date and set it to the dateTime variable
 
   const checkStatus = (e) => {
     let isActive = true;
-    if(dateTime === 'Thursday, 20 April 2022'){ //if the dateTime is Thursday, 20 April 2022, the bot will be inactive
+    if (dateTime === "Thursday, Aug 13 2022") {
+      //if the dateTime is Thursday, 13 Aug 2022, the bot will be inactive
       isActive = false;
     }
-    const status = document.querySelector('.status'); // selecting the status class
-    if(isActive === true){ //if the bot is active
-      status.innerHTML = 'Active';
-      status.style.color = 'green';
-    }else{
-      status.innerHTML = 'Inactive';
-      status.style.color = 'red';
+    const status = document.querySelector(".status");
+    // selecting the status class
+    if (isActive === true) {
+      //if the bot is active
+      status.innerHTML = "Active";
+      status.style.color = "green";
+    } else {
+      status.innerHTML = "Not Active";
+      status.style.color = "red";
     }
-  }
+  };
   const handleInput = () => {
-    const botMessage = document.querySelector('#message1');
-    const humanMessage = document.querySelector('#message2');
-    
-    let badwords = ['fuck|bad|stupid|useless|bitch|crazy|nonsense'] //adding the bad words
+    const botMessage = document.querySelector("#message1");
+    const userMessage = document.querySelector("#message2");
+    const inputRef = input.current;
+    const getHumanMessage = humanMessage.current;
+    const getBotMessage = botmessage.current;
+
+    let badwords = ["fuck|bad|stupid|useless|bitch|crazy|nonsense"];
     let words = new RegExp(badwords);
-    if(words.test(document.querySelector('#input').value)){ // if the input contains bad words
-      botMessage.innerHTML = 'Typing...';
+    if (words.test(document.querySelector("#input").value)) {
+      // if the input contains bad words
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'Please do not use bad words'; // display this message
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "Please do not use bad words"; // display the message
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
-
-    let welcome = ['Sup|sup|Hello|Hi|hello|hi|Yo|Halla|yello|yelow']
+    let welcome = [
+      "hi|hello|Hello|hey|sup|yo|wassup|whats up|howdy|greetings|good morning|good afternoon|good evening",
+    ];
     let words2 = new RegExp(welcome);
-    if(words2.test(document.querySelector('#input').value)){
-      const status = document.querySelector('.status');
-      botMessage.innerHTML = 'Typing...';
+    if (words2.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      // if the input contains welcome words
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'Hello There how are you doing today?';
-        status.innerHTML = 'Active';
-        status.style.color = 'green';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "Hello There how are you doing today?"; // display the message
+        status.innerText = "Active";
+        status.style.color = "green";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
-
-    let bye = ['Bye|bye|Goodbye|goodbye|See you later|see you later|See you|see you']
+    let bye = ["bye|Bye|goodbye|see you later|cya|goodnight|goodbye"];
     let words3 = new RegExp(bye);
-    if(words3.test(document.querySelector('#input').value)){
-      const status = document.querySelector('.status');
-      botMessage.innerHTML = 'Typing...';
+    if (words3.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'Bye, have a nice day';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "Bye, have a nice day";
+        inputRef.value = ""; // clear the input
       }, 2000);
       setTimeout(() => {
-        status.innerHTML = 'Not active';
-        status.style.color = 'red';
+        status.innerText = "Not active";
+        status.style.color = "red";
       }, 5000);
     }
-
-    let thanks = ['Thanks|thanks|thank you|thank you very much|thank you very much']
+    let thanks = [
+      "Thanks|thanks|thank you|thank you very much|Thank you very much",
+    ];
     let words4 = new RegExp(thanks);
-    if(words4.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words4.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'You are welcome';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "You are welcome";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
-
-    let how = ['How are you|how are you doing|how are you doing today|how are you doing today|How are you|how are you']
+    let how = [
+      "How are you|how are you doing|how are you doing today|how are you doing today|How are you|how are you",
+    ];
     let words5 = new RegExp(how);
-    if(words5.test(document.querySelector('#input').value)){
-      const status = document.querySelector('.status');
-      botMessage.innerHTML = 'Typing...';
+    if (words5.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'I am fine, thank you';
-        status.innerHTML = 'Active';
-        status.style.color = 'green';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "I am fine, thank you";
+        status.innerText = "Active";
+        status.style.color = "green";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
-
-    let good = ["That's good|Sound nice|that sounds awesome|that sounds great|Great|great|sounds great|that's sounds good|Nice|nice"]
+    let good = [
+      "That's good|Sound nice|that sounds awesome|that sounds great|Great|great|sounds great|that's sounds good|Nice|nice",
+    ];
     let words6 = new RegExp(good);
-    if(words6.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words6.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = '😁';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "😁";
+        inputRef.value = ""; // clear the input
       }, 1000);
     }
 
-    let response = ["I'm fine|I am fine|I am fine today|I am fine today|i'm fine|i'm great|I'm fine|I'm great|Good|good|I'm good|i'm good|great|Great"]
+    let response = [
+      "I'm fine|I am fine|I am fine today|I am fine today|i'm fine|i'm great|I'm fine|I'm great|I'm good|i'm good|great|Great",
+    ];
     let words7 = new RegExp(response);
-    if(words7.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words7.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'That is good';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "That is good";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
 
-    let nameAsk = ["What's your name|what's your name|What is your name|what is your name"]
+    let nameAsk = [
+      "What's your name|what's your name|What is your name|what is your name",
+    ];
     let words8 = new RegExp(nameAsk);
-    if(words8.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words8.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'My name is Bot';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "My name is Bot";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
 
-    let owner = ["Who is the owner|who is the owner|Who is the owner of this bot|who is the owner of this bot|Who made you|who made you|Who is your maker|Who made you|who is your maker|who is your owner|Who is your owner"]
+    let owner = [
+      "Who is the owner|who is the owner|Who is the owner of this bot|who is the owner of this bot|Who made you|who made you|Who is your maker|Who made you|who is your maker|who is your owner|Who is your owner",
+    ];
     let words9 = new RegExp(owner);
-    if(words9.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words9.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'The owner of this bot is Treasure';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "The owner of this bot is Treasure";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
 
-    let owner2 = ["Who's Treasure|who's Treasure|Who is Treasure|who is Treasure"]
+    let owner2 = [
+      "Who's Treasure|who's Treasure|Who is Treasure|who is Treasure",
+    ];
     let words10 = new RegExp(owner2);
-    if(words10.test(document.querySelector('#input').value)){
-      botMessage.innerHTML = 'Typing...';
+    if (words10.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'Treasure is a programmer based on ReactJS and NodeJS he is the owner of a youtube channel called Creative Tutorials';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText =
+          "Treasure is a programmer based on ReactJS and NodeJS he is the owner of a youtube channel called Creative Tutorials";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
 
-    let ageAsk = ["What's your age|what's your age|What is your age|what is your age|How old are you|how old are you"] //adding the age-question
+    let ageAsk = [
+      "What's your age|what's your age|What is your age|what is your age|How old are you|how old are you",
+    ]; //adding the age-question
     let words11 = new RegExp(ageAsk);
-    if(words11.test(document.querySelector('#input').value)){ // if the input contains some question
-      botMessage.innerHTML = 'Typing...';
+    if (words11.test(document.querySelector("#input").value)) {
+      // if the input contains some question
+      getBotMessage.innerText = "Typing...";
       setTimeout(() => {
-        botMessage.innerHTML = 'I am 1 year old';
-        document.querySelector('#input').value = '';
+        getBotMessage.innerText = "I am 1 year old";
+        inputRef.value = ""; // clear the input
       }, 2000);
     }
-    humanMessage.innerHTML = document.querySelector('#input').value; // display the input
-  }
+    getHumanMessage.innerText = inputRef.value; // display the message
+  };
   return (
     <div className="App" onLoad={checkStatus}>
       <div className="wrapper">
@@ -167,28 +215,40 @@ function App() {
             </div>
             <div className="right">
               <div className="name">ChatBot</div>
-              <div className='status'></div>
+              <div className="status">Active</div>
             </div>
           </div>
           <div className="main">
             <div className="main_content">
               <div className="messages">
-                <div className="bot-message" id='message1'></div>
-                <div className="human-message" id='message2'></div>
+                <div
+                  className="bot-message"
+                  id="message1"
+                  ref={botmessage}
+                ></div>
+                <div
+                  className="human-message"
+                  id="message2"
+                  ref={humanMessage}
+                ></div>
               </div>
             </div>
           </div>
           <div className="bottom">
             <div className="btm">
-                <div className="input">
-                  <input type="text" id='input' placeholder='Enter your message'/>
-                </div>
-                <div className="btn">
-                  <button onClick={handleInput}>
-                    <i className="fas fa-paper-plane"></i>
-                    Send
-                  </button>
-                </div>
+              <div className="input">
+                <input
+                  type="text"
+                  id="input"
+                  placeholder="Enter your message"
+                  ref={input}
+                />
+              </div>
+              <div className="btn">
+                <button onClick={handleInput}>
+                  <i className="fas fa-paper-plane"></i>Send
+                </button>
+              </div>
             </div>
           </div>
         </div>
